d4b684a04c2f2af1d9e5e94da22f25ddc9b91206
PM369 <pallabkumarmallick189@gmail.com>
2021-09-16 10:58:10 +0530

    api

--- src/components/Pages/Service.js
+++ src/components/Pages/Service.js
@@ -1,52 +1,45 @@
-import React, { useState } from 'react';
-// import data from './data'
-import data from "../data"
+import React, { useState,useEffect } from 'react';
+// import data from "../data"
 
 const Service = () => {
-    const [noOfElement, setnoOfElement] = useState(4);
-    const loadmore = () => {
-        setnoOfElement(noOfElement + noOfElement);
-    }
-
-    const slice = data.slice(0, noOfElement);
+    const [allData,setAllData]=useState([]);
+    const [petCount, setDataCount]= useState(4);
+    const loadmore =()=>{
+       setDataCount(allData.length)
+    };
+    useEffect(()=>{
+        fetch('http://127.0.0.1:8000/api/productlist')
+        .then ((res)=> res.json())
+        .then ((data)=>{
+            setAllData(data)
+        });
+    },[])
     return (
         <div className="section-bg">
-        <section id="Service" className="py-4 container">
-            {/* <h1 className="my-4 display-5 text-center">Our Services</h1> */}
-            <div className="section-title">
-                {/* <h2>Clients</h2> */}
-                <p>Our Service</p>
-            </div>
-            <div className="row justify-content-center">
-                {slice.map((item, index) => {
-                    return (
-                        <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
-                            <div className="card p-0 overflow-hidden h-100 shadow">
-                                <img src={item.img} className="card-img-top" alt="img" />
-                                <div className="card-body">
-                                    <h5 className="card-title">{item.title}</h5>
-                                    <p className="card-text">{item.desc}</p>
-
-                                </div>
+            <section id="Service" className="py-4 container">
+                <div className="section-title">
+                    {/* <h2>Clients</h2> */}
+                    <p>Our Service</p>
+                </div>
+                <div className="row justify-content-center" >
+                {allData.slice(0, petCount).map((pet, id) => (
+                    <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
+                    <div className="card p-0 overflow-hidden h-100 shadow">
+                            <img src={"http://127.0.0.1:8000/" + pet.ServiceImg} className="card-img-top" alt="img" />
+                            <div className="card-body">
+                                <h5 className="card-title">{pet.ServiceName}</h5>
+                                <p className="card-text">{pet.ServiceDesc}</p>
 
                             </div>
-
                         </div>
-
-                    )
-                })}
-
-
+                   </div>))}
             </div>
-
             <button className="btn-get-service"
-                onClick={() => loadmore()}
+             onClick={() => loadmore()}
             >
                 Lodemore
             </button>
-
-
-        </section>
+            </section>
         </div>
     );
 };
